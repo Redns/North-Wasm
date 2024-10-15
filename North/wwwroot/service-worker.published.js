0196bd8090dd6bc908d02e32e4a4dd25751eefea
Redns <ljing431@163.com>
2024-10-15 19:03:30 +0800

    ddd

--- North/wwwroot/service-worker.published.js
+++ North/wwwroot/service-worker.published.js
@@ -19,12 +19,12 @@ const manifestUrlList = self.assetsManifest.assets.map(asset => new URL(asset.ur
 async function onInstall(event) {
     console.info('Service worker: Install');
 
-    // // Fetch and cache all matching items from the assets manifest
-    // const assetsRequests = self.assetsManifest.assets
-    //     .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
-    //     .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-    //     .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
-    // await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    // Fetch and cache all matching items from the assets manifest
+    const assetsRequests = self.assetsManifest.assets
+        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+        .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+    await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
 async function onActivate(event) {
